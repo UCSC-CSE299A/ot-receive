501cd6037588bcb276d39080b0a4840ab1a27482
Simeon Tran <siatran@ucsc.edu>
2024-02-02 14:58:29 -0800

    Comment

--- components/ot_receive/receive_udp.c
+++ components/ot_receive/receive_udp.c
@@ -1,9 +1,13 @@
 #include "ot_receive.h"
 
+/**
+ * Only handle messages sent by the `ot-send` UDP sender, which broadcast
+ * packets at MLEID_MULTICAST with port 12345.
+*/
 bool udpReceiveCallback(void *aContext,
                         const otMessage *aMessage,
                         const otMessageInfo *aMessageInfo)
-{     
+{
   otIp6Address *ipPeer = &(aMessageInfo->mPeerAddr);
   char *ipPeerString = calloc(1, OT_IP6_SOCK_ADDR_STRING_SIZE);
   otIp6AddressToString(ipPeer, ipPeerString, OT_IP6_SOCK_ADDR_STRING_SIZE);
