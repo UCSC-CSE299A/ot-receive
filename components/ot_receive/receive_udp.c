d058b3bbcd48bebe3c4e1402f7e941a934acf69e
Simeon Tran <siatran@ucsc.edu>
2024-02-02 16:11:05 -0800

    Taking a break for now. I have the message; it's just a matter of reading it.

--- components/ot_receive/receive_udp.c
+++ components/ot_receive/receive_udp.c
@@ -1,10 +1,13 @@
 #include "ot_receive.h"
 #include <string.h>
 
-void udpGetPayload(const otMessage *aMessage, char *buffer) {
-  uint16_t payloadStart = otMessageGetOffset(aMessage) - UDP_PAYLOAD_SIZE;
-  otMessageRead(aMessage, payloadStart, buffer, UDP_PAYLOAD_SIZE);
-  return;
+void* udpGetPayload(const otMessage *aMessage) {
+  uint16_t offset = otMessageGetOffset(aMessage) - UDP_PAYLOAD_SIZE;
+  void* buffer = calloc(1, UDP_PAYLOAD_SIZE);
+  uint16_t bytesRead = otMessageRead(aMessage, offset, buffer, UDP_PAYLOAD_SIZE);
+
+  DEBUG_PRINT(otLogNotePlat("Bytes read: %d", (int) bytesRead));
+  return buffer;
 }
 
 /**
@@ -19,10 +22,16 @@ bool udpReceiveCallback(void *aContext,
   uint16_t sockPort = aMessageInfo->mSockPort;
 
   if ((peerPort == UDP_DEST_PORT) && (sockPort == UDP_SOCK_PORT)) {
-    char* payload = calloc(1, UDP_PAYLOAD_SIZE);
-    udpGetPayload((const otMessage *) aMessage, payload);
+    char* payload = (char *) udpGetPayload((const otMessage *) aMessage);
 
-    DEBUG_PRINT(otLogNotePlat("Recevied UDP Packet: \"%s\"", payload));
+    otLogNotePlat("--------------------");
+    for (char c = 0; c < otMessageGetOffset(aMessage); c++) {
+      otLogNotePlat("%c", payload[c]);
+    }
+    otLogNotePlat("--------------------");
+
+    // DEBUG_PRINT(otLogNotePlat("%x", payload));
+    free(payload);
     return true;
   }
   return false;
