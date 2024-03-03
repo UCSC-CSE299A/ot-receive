793a0fad292c1f933d414475c90fbe87480c399e
Simeon Tran <siatran@ucsc.edu>
2024-03-03 15:05:24 -0800

    String representation of uint32_t is 10 bytes, not 4.

--- components/ot_receive/include/ot_receive.h
+++ components/ot_receive/include/ot_receive.h
@@ -53,11 +53,14 @@
  *    "Packet Number [uint_32t]"
  *
  * Substring "Packet Number " makes up 14 bytes.
- * A 32 bit integers is 4 bytes long.
  *
- * Thus, the total payload isze is 14 + 4 = 18 bytes.
+ * The 32 bit integer will be represented as a string.
+ * 2^32 - 1 = 4294967295 has 10 digits, and thus, the
+ * string representation of the uint32_t will make up 10 bytes.
+ *
+ * Thus, the total payload isze is 14 + 10 = 24 bytes.
 */
-#define PAYLOAD_SIZE 18
+#define PAYLOAD_SIZE 24
 
 #define OUTPUT_STRING_SIZE strlen("Received ") + PAYLOAD_SIZE
 
